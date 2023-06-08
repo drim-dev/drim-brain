7445135ed9d075cfef12bf7f43013eb6fdd60b20
Dmitriy Melnik <dmtr.melnik@gmail.com>
2023-06-08 20:41:08 +0600

    #blockchain rename Password in User

--- blockchain/BlockchainLab/src/WebApi/Features/Users/Domain/User.cs
+++ blockchain/BlockchainLab/src/WebApi/Features/Users/Domain/User.cs
@@ -4,7 +4,7 @@ public class User
 {
     public Guid Id { get; set; }
     public string Email { get; set; }
-    public string Password { get; set; }
+    public string PasswordHash { get; set; }
     public DateTime DateOfBirth { get; set; }
     public DateTime RegisteredAt { get; set; }
 }
