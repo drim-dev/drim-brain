ac8992d5eeee82b5876490f410e3cd15a681ee2f
mitro <dmtr.melnik@gmail.com>
2023-11-30 11:05:44 +0600

    #tcp: implement TcpLab

---
+++ networking/tcp-ip/tcp/_lab/src/TcpClient/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Enter a request to send to the server, or 'quit' to exit");
+
+while (true)
+{
+    var request = Console.ReadLine();
+
+    if (request == "quit")
+    {
+        break;
+    }
+
+    var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+    await client.ConnectAsync(IPAddress.Loopback, 15000);
+
+    var requestBytes = Encoding.UTF8.GetBytes(request);
+
+    await client.SendAsync(requestBytes, SocketFlags.None);
+
+    var buffer = new byte[1024];
+
+    if (request.StartsWith("DOWNLOAD "))
+    {
+        await client.ReceiveAsync(buffer, SocketFlags.None);
+        var response = Encoding.UTF8.GetString(buffer);
+
+        if (response.StartsWith("OK\n\n"))
+        {
+            var length = long.Parse(response[5..].Split('\n')[0].Split(' ')[1]);
+
+            Directory.CreateDirectory("Downloads");
+
+            var fileName = request[9..];
+            await using var file = File.Open($"Downloads/{fileName}", FileMode.Create, FileAccess.Write, FileShare.Write);
+
+            var receivedBytes = 0L;
+
+            while (receivedBytes < length)
+            {
+                var bytesRead = await client.ReceiveAsync(buffer, SocketFlags.None);
+
+                await file.WriteAsync(buffer.AsMemory(0, bytesRead));
+
+                receivedBytes += bytesRead;
+            }
+
+            Console.WriteLine($"File {fileName} downloaded successfully\n");
+        }
+        else
+        {
+            Console.WriteLine($"\n{response}");
+        }
+    }
+    else
+    {
+        var bytesRead = await client.ReceiveAsync(buffer, SocketFlags.None);
+
+        var response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+        Console.WriteLine($"\n{response}");
+    }
+
+    client.Close();
+}
+
+Console.WriteLine("Goodbye!");
---
+++ networking/tcp-ip/tcp/_lab/src/TcpServer/Program.cs
@@ -0,0 +1,119 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var cts = new CancellationTokenSource();
+
+Console.CancelKeyPress += (_, args) =>
+{
+    cts.Cancel();
+    args.Cancel = true;
+};
+
+var listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+listener.Bind(new IPEndPoint(IPAddress.Loopback, 15000));
+
+listener.Listen(10);
+
+while (!cts.IsCancellationRequested)
+{
+    try
+    {
+        var socket = await listener.AcceptAsync(cts.Token);
+
+        var buffer = new byte[1024];
+
+        var bytesRead = await socket.ReceiveAsync(buffer, SocketFlags.None, cts.Token);
+
+        var request = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+        Console.WriteLine($"Received request: {request}");
+
+        if (request.StartsWith("DOWNLOAD "))
+        {
+            var fileName = request[9..];
+
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                await socket.SendAsync("BAD REQUEST\n"u8.ToArray(), SocketFlags.None, cts.Token);
+            }
+            else if (!File.Exists($"Files/{fileName}"))
+            {
+                await socket.SendAsync("NOT FOUND\n"u8.ToArray(), SocketFlags.None, cts.Token);
+            }
+            else
+            {
+                var fileInfo = new FileInfo($"Files/{fileName}");
+
+                var sb = new StringBuilder("OK\n\n");
+                sb.AppendLine($"LENGTH: {fileInfo.Length}");
+
+                await socket.SendAsync(Encoding.UTF8.GetBytes(sb.ToString()), SocketFlags.None, cts.Token);
+
+                await using var file = fileInfo.Open(FileMode.Open, FileAccess.Read, FileShare.Read);
+
+                var sendBuffer = new byte[1024];
+
+                while (true)
+                {
+                    bytesRead = await file.ReadAsync(sendBuffer, cts.Token);
+
+                    if (bytesRead == 0)
+                    {
+                        break;
+                    }
+
+                    await socket.SendAsync(sendBuffer.AsMemory()[..bytesRead], SocketFlags.None, cts.Token);
+                }
+            }
+        }
+        else if (request.StartsWith("LIST"))
+        {
+            var sb = new StringBuilder("OK\n\n");
+            foreach (var file in Directory.EnumerateFiles("Files"))
+            {
+                sb.AppendLine(file[6..]);
+            }
+
+            var responseBytes = Encoding.UTF8.GetBytes(sb.ToString());
+            await socket.SendAsync(responseBytes, SocketFlags.None, cts.Token);
+        }
+        else if (request.StartsWith("SEARCH "))
+        {
+            var term = request[7..];
+
+            if (string.IsNullOrWhiteSpace(term))
+            {
+                await socket.SendAsync("BAD REQUEST\n"u8.ToArray(), SocketFlags.None, cts.Token);
+            }
+            else
+            {
+                var sb = new StringBuilder("OK\n\n");
+                foreach (var file in Directory.EnumerateFiles("Files").Where(f => f.Contains(term)))
+                {
+                    sb.AppendLine(file[6..]);
+                }
+
+                var responseBytes = Encoding.UTF8.GetBytes(sb.ToString());
+                await socket.SendAsync(responseBytes, SocketFlags.None, cts.Token);
+            }
+        }
+        else
+        {
+            await socket.SendAsync("BAD REQUEST\n"u8.ToArray(), SocketFlags.None, cts.Token);
+        }
+
+        socket.Close();
+    }
+    catch (OperationCanceledException)
+    {
+        break;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Exception: {ex}");
+    }
+}
+
+Console.WriteLine("Goodbye!");
